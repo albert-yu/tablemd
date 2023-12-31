fd1161eb1ab991a5653db23319faf08e7b8263dd
Albert Yu <albert.yu@gmail.com>
2023-12-30 21:09:32 -0500

    Add a regez.h

--- build.zig
+++ build.zig
@@ -23,6 +23,9 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.addIncludePath(.{
+        .path = "lib",
+    });
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
