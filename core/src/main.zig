bf7a5cd341cc58f5848cc292c134d39ebb489438
Albert Yu <albert.yu@gmail.com>
2024-01-17 22:44:02 -0500

    Rename CellExpr -> Expr

--- core/src/main.zig
+++ core/src/main.zig
@@ -5,7 +5,7 @@ const print = std.debug.print;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
-    var parsed = try parser.CellExpr.parse(allocator, "$A3=TRUE");
+    var parsed = try parser.Expr.parse(allocator, "$A3=TRUE");
     defer parsed.destroySelf(allocator);
     const sexpr = try parsed.toSexpr(allocator);
     defer allocator.free(sexpr);
