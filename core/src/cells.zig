62a04907f78ef767a808a3170008d0a2dddf25f1
Albert Yu <albert.yu@gmail.com>
2024-03-05 21:37:13 -0500

    Add generated .set impl

--- core/src/cells.zig
+++ core/src/cells.zig
@@ -104,4 +104,46 @@ pub const QT4 = struct {
         }
         return tile_3[index_4];
     }
+
+    /// Right now, invalid indexes are no-ops
+    pub fn set(self: QT4, allocator: std.mem.Allocator, row: usize, col: usize, data: Data) !void {
+        if (SIZEW <= col or SIZEH <= row) {
+            return;
+        }
+        const index_1 = compute_index(row, col, 3);
+        if (index_1 >= self.root.len) {
+            return;
+        }
+        var tile_1 = self.root[index_1];
+        if (tile_1.len == 0) {
+            const new_tile_1 = try allocator.alloc(Tile2, W * H);
+            self.allocs_tile_1.append(new_tile_1);
+            tile_1.* = new_tile_1;
+        }
+        const index_2 = compute_index(row, col, 2);
+        if (index_2 >= tile_1.len) {
+            return;
+        }
+        var tile_2 = tile_1[index_2];
+        if (tile_2.len == 0) {
+            const new_tile_2 = try allocator.alloc(Tile3, W * H);
+            self.allocs_tile_2.append(new_tile_2);
+            tile_1[index_2] = new_tile_2;
+        }
+        const index_3 = compute_index(row, col, 1);
+        if (index_3 >= tile_2.len) {
+            return;
+        }
+        const tile_3 = tile_2[index_3];
+        if (tile_3.len == 0) {
+            const new_tile_3 = try allocator.alloc(Data, W * H);
+            self.allocs_tile_3.append(new_tile_3);
+            tile_2[index_3] = new_tile_3;
+        }
+        const index_4 = compute_index(row, col, 0);
+        if (index_4 >= tile_3.len) {
+            return;
+        }
+        tile_3[index_4] = data;
+    }
 };
