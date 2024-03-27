d3533860248783ecb3d7d826234e0098398ab07c
Albert Yu <albert.yu@gmail.com>
2024-03-26 21:16:03 -0400

    Output wasm build to server/build

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p build
-zig build-lib core/src/lib.zig -target wasm32-freestanding -fno-entry --export=newSheet -femit-bin=build/core.wasm
+mkdir -p server/build
+zig build-lib core/src/lib.zig -target wasm32-freestanding -fno-entry --export=newSheet -femit-bin=server/build/core.wasm
