5059bd0c1234921990c98663de5768c383cfe6b8
Albert Yu <albert.yu@gmail.com>
2024-03-27 23:09:13 -0400

    invoke build-exe NOT build-lib

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir -p server/build
-zig build-lib core/src/lib.zig -target wasm32-freestanding -OReleaseSmall -femit-bin=server/build/core.o
-wasm-ld server/build/core.o -o server/build/core.wasm -O2 --no-entry --allow-undefined
+zig build-exe core/src/lib.zig -target wasm32-freestanding -fno-entry --export=newSheet -femit-bin=server/build/core.wasm
