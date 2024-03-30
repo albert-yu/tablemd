709bcc4c5217e16908185f41dde616c7ce92c7a2
Albert Yu <albert.yu@gmail.com>
2024-03-30 12:17:46 -0400

    Remove perhaps unncessary args

--- scripts/build-wasm.sh
+++ scripts/build-wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p web/build
-zig build-lib core/src/lib.zig -target wasm32-freestanding -fno-entry -rdynamic -fallow-shlib-undefined -femit-bin=web/build/core.wasm $1 $2 $3
+zig build-lib core/src/lib.zig -target wasm32-freestanding -femit-bin=web/build/core.wasm $1 $2 $3
 wasm-ld --no-entry --export-dynamic --export-all --allow-undefined --import-memory -o web/build/core.wasm web/build/core.wasm.o
