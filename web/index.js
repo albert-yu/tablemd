61d5198fbb1a05331c9f269c1ce06b2c75ab0ff4
Albert Yu <albert.yu@gmail.com>
2024-03-31 23:33:17 -0400

    Use upper snake consts for h/w

--- web/index.js
+++ web/index.js
@@ -3,6 +3,9 @@ const getColorIndicesForCoord = (x, y, width) => {
   return [red, red + 1, red + 2, red + 3];
 };
 
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+
 try {
   /**
    * This gets assigned later
@@ -12,14 +15,9 @@ try {
   // Set up the canvas with a 2D rendering context
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
-  // const bcr = canvas.getBoundingClientRect();
-  // const width = bcr.width;
-  // const height = bcr.height;
-  const width = 500;
-  const height = 500;
-  canvas.height = height;
-  canvas.width = width;
-  const size = width * height;
+  canvas.height = CANVAS_HEIGHT;
+  canvas.width = CANVAS_WIDTH;
+  const size = CANVAS_WIDTH * CANVAS_HEIGHT;
   const byteSize = size * 4;
 
   ctx.imageSmoothingEnabled = false;
@@ -41,7 +39,7 @@ try {
   const exports = result.instance.exports;
   const initApp = exports.app_init;
   const deinitApp = exports.app_deinit;
-  const app = initApp(width, height, 1);
+  const app = initApp(CANVAS_WIDTH, CANVAS_HEIGHT, 1);
   memory = exports.memory;
   const canvasBufferOffset = exports.get_canvas_buffer_ptr(app);
   const canvasData = new Uint8ClampedArray(
@@ -49,7 +47,7 @@ try {
     canvasBufferOffset,
     byteSize,
   );
-  const imageData = new ImageData(canvasData, width);
+  const imageData = new ImageData(canvasData, CANVAS_WIDTH);
   ctx.putImageData(imageData, 0, 0);
 
   deinitApp(app);
