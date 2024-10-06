0c25369c37dd24b98a0a40ab3c9ff613bb6422da
Albert Yu <albert.yu@gmail.com>
2024-10-06 18:28:04 -0400

    Remove dead code in main

--- web/src/client/main.ts
+++ web/src/client/main.ts
@@ -4,18 +4,6 @@ import { Vec4 } from "./Vec4";
 import { SAMPLE_COUNT } from "./constants";
 import { UIRenderer } from "./ui-renderer";
 
-type Interval = [number, number];
-
-type ScaleDomainAndRange = {
-  domain: Interval;
-  range: Interval;
-};
-
-type Scales = {
-  x: ScaleDomainAndRange;
-  y: ScaleDomainAndRange;
-};
-
 const cursorStyle = {
   select: "auto",
   pan: "grab",
@@ -205,90 +193,6 @@ async function main() {
   } catch (err) {
     console.log(err);
   }
-}
-
-function window_transform(scales: Scales, width: number, height: number) {
-  // A function that creates the two matrices a webgl shader needs, in addition to the zoom state,
-  // to stay aligned with canvas and d3 zoom.
-
-  // width and height are svg parameters; x and y scales project from the data x and y into the
-  // the webgl space.
-
-  // Given two d3 scales in coordinate space, create two matrices that project from the original
-  // space into [-1, 1] webgl space.
-
-  // return the magnitude of a scale.
-  const x_domain = scales.x.domain;
-  const y_domain = scales.y.domain;
-  const x_range = scales.x.range;
-  const y_range = scales.y.range;
-  let x_domain_mid = mean(x_domain);
-  let y_domain_mid = mean(y_domain);
-  const x_range_mid = mean(x_range);
-  const y_range_mid = mean(y_range);
-  let xmulti = gap(x_range) / gap(x_domain);
-  let ymulti = gap(y_range) / gap(y_domain);
-
-  // translates from data space to scaled space.
-  // prettier-ignore
-  const m1 = [
-    xmulti, 0, 0, 0,
-    0, ymulti, 0, 0,
-    0, 0, 1, 0,
-    -xmulti * x_domain_mid + x_range_mid,
-    -ymulti * y_domain_mid + y_range_mid,
-    0,
-    1,
-  ];
-
-  // translate from scaled space to webgl space.
-  // The '2' here is because webgl space runs from -1 to 1; the shift at the end is to
-  // shift from [0, 2] to [-1, 1]
-  // prettier-ignore
-  const m2 = [
-    2 / width, 0, 0, 0, // First column
-    0, -2 / height, 0, 0, // Second column
-    0, 0, 1, 0, // Third column (unchanged for z-axis in 2D transformations)
-    -1, 1, 0, 1, // Fourth column, with translations adjusted for WebGL space
-  ];
-
-  return [m1, m2];
-}
-
-/**
- * Returns [min, max].
- *
- * Adapted from
- * https://github.com/d3/d3-array/blob/be0ae0d2b36ab91b833294ad2cfc5d5905acbd0f/src/extent.js#L1
- */
-function extent(values: Float32Array): [number, number] {
-  let min: number | undefined = undefined;
-  let max: number | undefined = undefined;
-  for (const value of values) {
-    if (value != null) {
-      if (min === undefined) {
-        if (value >= value) {
-          min = max = value;
-        }
-      } else {
-        if (min > value) {
-          min = value;
-        }
-        if (max! < value) {
-          max = value;
-        }
-      }
-    }
-  }
-  return [min!, max!];
-}
-
-function mean([low, hi]: Interval) {
-  return (hi - low) / 2;
-}
-
-function gap(arr: Interval) {
-  return arr[1] - arr[0];
 }
 
 function getCanvasSelectMode() {
