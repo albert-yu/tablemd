4f8c8be5880ab7c076924ad686e87c512146655c
Albert Yu <albert.yu@gmail.com>
2025-01-16 18:24:27 -0500

    Just use f32

--- core/src/lib.zig
+++ core/src/lib.zig
@@ -1,16 +1,15 @@
 const std = @import("std");
 const engine = @import("engine.zig");
+const rect_render = @import("render/rect.zig");
 
 const Sheet = engine.Sheet;
-
-const float = f32;
 
 /// Don't use directly, use consoleLog instead
 extern fn print(ptr: [*]const u8, len: u32) void;
 
 extern fn print_u32(value: u32) void;
 
-extern fn print_float(value: float) void;
+extern fn print_float(value: f32) void;
 
 /// Wrapper around `print` to make it easier to use
 fn consoleLog(str: []const u8) void {
@@ -23,8 +22,8 @@ const CellPosition = struct {
 };
 
 const CellDimensions = struct {
-    width: float,
-    height: float,
+    width: f32,
+    height: f32,
 };
 
 const DEFAULT_SIGMA = 1e-6;
@@ -32,18 +31,18 @@ const GRID_N = 1000;
 const GRID_DENSITY: comptime_float = 1.0 / 32.0;
 
 const Point2D = struct {
-    x: float,
-    y: float,
+    x: f32,
+    y: f32,
 };
 
 const RectElement = struct {
-    color: [4]float,
-    x: float,
-    y: float,
-    width: float,
-    height: float,
-    corners: [4]float,
-    sigma: float,
+    color: [4]f32,
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    corners: [4]f32,
+    sigma: f32,
 };
 
 /// Represents the running application
@@ -80,8 +79,8 @@ const App = struct {
                 .y = 0,
                 .width = grid_cell_width,
                 .height = grid_cell_height,
-                .color = [4]float{ 0, 0, 0, 0.25 },
-                .corners = [4]float{ 0, 0, 0, 0 },
+                .color = [4]f32{ 0, 0, 0, 0.25 },
+                .corners = [4]f32{ 0, 0, 0, 0 },
                 .sigma = DEFAULT_SIGMA,
             },
         };
@@ -109,11 +108,11 @@ const App = struct {
         // TODO: update cell width and height based on underlying content
         // e.g. text, cell size
         const cell_dims = self.cellDimensions();
-        self.hover_rect.x = cell_dims.width * @as(float, @floatFromInt(cell.col));
-        self.hover_rect.y = cell_dims.height * @as(float, @floatFromInt(cell.row));
+        self.hover_rect.x = cell_dims.width * @as(f32, @floatFromInt(cell.col));
+        self.hover_rect.y = cell_dims.height * @as(f32, @floatFromInt(cell.row));
     }
 
-    pub fn writeHoverRect(self: *App, float_array: [*c]float, offset: usize) void {
+    pub fn writeHoverRect(self: *App, float_array: [*c]f32, offset: usize) void {
         const UNUSED = 0.0;
         const rect = self.hover_rect;
         float_array[offset] = rect.color[0];
@@ -137,7 +136,7 @@ const App = struct {
     fn normalizePoint(self: *App, canvasPoint: Point2D) Point2D {
         const maxWidth = self.canvas_width;
         const maxHeight = self.canvas_height;
-        const maxGridDim = @as(float, @floatFromInt(@min(maxWidth, maxHeight)));
+        const maxGridDim = @as(f32, @floatFromInt(@min(maxWidth, maxHeight)));
         const gridX = canvasPoint.x / maxGridDim;
         const gridY = canvasPoint.y / maxGridDim;
         return Point2D{
@@ -167,7 +166,7 @@ const App = struct {
         };
     }
 
-    fn getIndexOfMaxGridPointBoundedBy(self: App, upperBound: float) u32 {
+    fn getIndexOfMaxGridPointBoundedBy(self: App, upperBound: f32) u32 {
         var i: u32 = 0;
         while (i < GRID_N) : (i += 1) {
             const val = self.grid_x[i];
@@ -206,19 +205,19 @@ export fn app_set_canvas_size(app: *App, width: usize, height: usize) void {
     app.setCanvasSize(width, height);
 }
 
-export fn app_on_hover(app: *App, x: float, y: float) void {
+export fn app_on_hover(app: *App, x: f32, y: f32) void {
     app.onHover(Point2D{ .x = x, .y = y });
 }
 
-export fn app_write_hover_rect(app: *App, float_array: [*c]float, offset: usize) void {
+export fn app_write_hover_rect(app: *App, float_array: [*c]f32, offset: usize) void {
     app.writeHoverRect(float_array, offset);
 }
 
 fn createXArray(allocator: std.mem.Allocator) ![]f32 {
-    const array = try allocator.alloc(float, GRID_N * GRID_N);
+    const array = try allocator.alloc(f32, GRID_N * GRID_N);
     var i: usize = 0;
     while (i < GRID_N * GRID_N) : (i += 1) {
-        const numer = @as(float, @floatFromInt(i % GRID_N));
+        const numer = @as(f32, @floatFromInt(i % GRID_N));
         const denom = GRID_N * GRID_DENSITY;
         array[i] = numer / denom;
     }
@@ -226,10 +225,10 @@ fn createXArray(allocator: std.mem.Allocator) ![]f32 {
 }
 
 fn createYArray(allocator: std.mem.Allocator) ![]f32 {
-    const array = try allocator.alloc(float, GRID_N * GRID_N);
+    const array = try allocator.alloc(f32, GRID_N * GRID_N);
     var i: usize = 0;
     while (i < GRID_N * GRID_N) : (i += 1) {
-        const numer = @as(float, @floatFromInt(i / GRID_N));
+        const numer = @as(f32, @floatFromInt(i / GRID_N));
         const denom = GRID_N * GRID_DENSITY;
         array[i] = numer / denom;
     }
