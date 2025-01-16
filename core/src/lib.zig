34b2ac89bc20f3b0909b931167c9ca1225f9d364
Albert Yu <albert.yu@gmail.com>
2025-01-15 23:49:44 -0500

    Remove unnecessary float casts

--- core/src/lib.zig
+++ core/src/lib.zig
@@ -219,7 +219,7 @@ fn createXArray(allocator: std.mem.Allocator) ![]f32 {
     var i: usize = 0;
     while (i < GRID_N * GRID_N) : (i += 1) {
         const numer = @as(float, @floatFromInt(i % GRID_N));
-        const denom = @as(float, @floatFromInt(GRID_N)) * GRID_DENSITY;
+        const denom = GRID_N * GRID_DENSITY;
         array[i] = numer / denom;
     }
     return array;
@@ -230,7 +230,7 @@ fn createYArray(allocator: std.mem.Allocator) ![]f32 {
     var i: usize = 0;
     while (i < GRID_N * GRID_N) : (i += 1) {
         const numer = @as(float, @floatFromInt(i / GRID_N));
-        const denom = @as(float, @floatFromInt(GRID_N)) * GRID_DENSITY;
+        const denom = GRID_N * GRID_DENSITY;
         array[i] = numer / denom;
     }
     return array;
