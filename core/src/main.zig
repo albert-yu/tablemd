b7697f23becf95cb4ba4aa9f9c17aa0fcde478b2
Albert Yu <albert.yu@gmail.com>
2025-02-13 23:13:18 -0500

    Update zoom matrix members only

--- core/src/main.zig
+++ core/src/main.zig
@@ -64,12 +64,16 @@ const state = struct {
     var grid_pos: [GRID_N * GRID_N]Vec2 = undefined;
 
     fn updateZoom(k: f32, x: f32, y: f32) void {
-        state.zoom.m = [4][4]f32{
-            .{ k, 0.0, 0.0, 0.0 },
-            .{ 0.0, k, 0.0, 0.0 },
-            .{ 0.0, 0.0, 1.0, 0.0 },
-            .{ x, y, 0.0, 1.0 },
-        };
+        // state.zoom.m = [4][4]f32{
+        //     .{ k, 0.0, 0.0, 0.0 },
+        //     .{ 0.0, k, 0.0, 0.0 },
+        //     .{ 0.0, 0.0, 1.0, 0.0 },
+        //     .{ x, y, 0.0, 1.0 },
+        // };
+        state.zoom.m[0][0] = k;
+        state.zoom.m[1][1] = k;
+        state.zoom.m[3][0] = x;
+        state.zoom.m[3][1] = y;
     }
 
     fn getZoom() Zoom {
