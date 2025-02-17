45ec2cfa08f0de61f8803a12767c7ad5b950d2c3
Albert Yu <albert.yu@gmail.com>
2025-02-17 17:17:24 -0500

    Split out transform matrix

--- core/src/render/text.zig
+++ core/src/render/text.zig
@@ -10,7 +10,10 @@ const font_atlas = @embedFile("../render/fonts/space-mono-regular/atlas.png");
 const atlas_w = 512;
 const atlas_h = 512;
 
+const char_count = font.chars.len;
+
 const TEXT_N = 1024;
+const CHAR_N = 1024;
 
 const sg = sokol.gfx;
 const sgl = sokol.gl;
@@ -33,6 +36,9 @@ pub const Renderer = struct {
     bind: sg.Bindings,
     pip: sg.Pipeline,
 
+    char_elements: [CHAR_N]CharElement,
+    text_elements: [TEXT_N]TextElement,
+
     pub fn new() Renderer {
         return .{
             .bind = .{},
@@ -41,8 +47,12 @@ pub const Renderer = struct {
     }
 
     pub fn setup(self: *Renderer) void {
+        const BUF_vert = 0;
+        const BUF_text = 1;
+        const BUF_char = 2;
+
         // set up quad
-        self.bind.vertex_buffers[0] = sg.makeBuffer(.{
+        self.bind.vertex_buffers[BUF_vert] = sg.makeBuffer(.{
             .data = sg.asRange(&[_]f32{
                 0, -1,
                 1, -1,
@@ -58,10 +68,100 @@ pub const Renderer = struct {
         });
 
         // set up font data
-        self.bind.vertex_buffers[1] = sg.makeBuffer(.{
+        self.bind.vertex_buffers[BUF_text] = sg.makeBuffer(.{
             .size = TEXT_N * @sizeOf(TextElement),
             .usage = .STREAM,
         });
+
+        self.bind.vertex_buffers[BUF_char] = sg.makeBuffer(.{
+            .usage = .STREAM,
+            .size = char_count * @sizeOf(CharElement),
+        });
+
+        var pip: sg.PipelineDesc = .{
+            .shader = sg.makeShader(shd.textShaderDesc(sg.queryBackend())),
+            .layout = init: {
+                var l = sg.VertexLayoutState{};
+                l.buffers[BUF_text].step_func = .PER_INSTANCE;
+                l.buffers[BUF_char].step_func = .PER_INSTANCE;
+                l.attrs[shd.ATTR_text_position] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_vert,
+                };
+                l.attrs[shd.ATTR_text_tex_offset] = .{
+                    .format = .FLOAT2,
+                    .buffer_index = BUF_char,
+                };
+                l.attrs[shd.ATTR_text_tex_extent] = .{
+                    .format = .FLOAT2,
+                    .buffer_index = BUF_char,
+                };
+                l.attrs[shd.ATTR_text_size] = .{
+                    .format = .FLOAT2,
+                    .buffer_index = BUF_char,
+                };
+                l.attrs[shd.ATTR_text_offset] = .{
+                    .format = .FLOAT2,
+                    .buffer_index = BUF_char,
+                };
+                l.attrs[shd.ATTR_text_transform0] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_transform1] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_transform2] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_transform3] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_color0] = .{
+                    .format = .FLOAT4,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_scale] = .{
+                    .format = .FLOAT1,
+                    .buffer_index = BUF_text,
+                };
+                l.attrs[shd.ATTR_text_char] = .{
+                    .format = .FLOAT3,
+                    .buffer_index = BUF_text,
+                };
+                break :init l;
+            },
+            .index_type = .UINT16,
+            .depth = .{
+                .compare = .LESS_EQUAL,
+                .write_enabled = true,
+            },
+        };
+        pip.colors[0].blend = .{
+            .enabled = true,
+            .src_factor_alpha = .SRC_ALPHA,
+            .dst_factor_alpha = .ONE_MINUS_SRC_ALPHA,
+            .src_factor_rgb = .SRC_ALPHA,
+            .dst_factor_rgb = .ONE_MINUS_SRC_ALPHA,
+        };
+
+        self.pip = pip;
+
+        // populate char elements
+        const u = 1.0 / font.common.scale_w;
+        const v = 1.0 / font.common.scale_h;
+        for (0..char_count) |i| {
+            const char = font.chars[i];
+            self.char_elements[i] = .{
+                .tex_offset = Vec2.new(char.x * u, char.y * v),
+                .tex_extent = Vec2.new(char.width * u, char.height * v),
+                .size = Vec2.new(char.width, char.height),
+                .offset = Vec2.new(char.xoffset, char.yoffset),
+            };
+        }
 
         // set up atlas texture
         self.bind.images[shd.IMG_tex] = sg.allocImage();
--- core/src/shaders/text.glsl
+++ core/src/shaders/text.glsl
@@ -22,7 +22,11 @@ in vec2 offset;
 // end
 
 // struct FormattedText
-in mat4 transform;
+// TODO: some way to input the whole matrix at once?
+in vec4 transform0;
+in vec4 transform1;
+in vec4 transform2;
+in vec4 transform3;
 in vec4 color0;
 in float scale;
 in vec3 char;
@@ -36,7 +40,11 @@ void main() {
     vec2 char_pos = (position.xy * size + text_el.xy + offset) * scale;
     vec4 char_pos4 = vec4(char_pos, 0.0, 1.0);
     mat4 t = untransform * window_scale * zoom;
-    gl_Position = t * transform * position;
+    float p0 = dot(transform0, position);
+    float p1 = dot(transform1, position);
+    float p2 = dot(transform2, position);
+    float p3 = dot(transform3, position);
+    gl_Position = t * vec4(p0, p1, p2, p3);
     // #ifndef SOKOL_WGSL
     // gl_PointSize = psize;
     // #endif
