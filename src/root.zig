af7667aa9a39d3d5a40f9646b6bdf1a95e421f58
Albert Yu <albert.yu@gmail.com>
2025-07-17 21:41:10 -0400

    Add example add(a, b) function
    Just testing out calling Zig from JS

--- src/root.zig
+++ src/root.zig
@@ -142,6 +142,10 @@ pub fn main() void {
     });
 }
 
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
 export fn input(ev: ?*const sapp.Event) void {
     const event = ev.?;
     switch (event.type) {
