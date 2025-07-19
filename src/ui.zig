55752a45cb6938b71eb9249ae548f22f5c5ccc86
Albert Yu <albert.yu@gmail.com>
2025-07-19 15:53:53 -0400

    Remove hovered cursor from state

--- src/ui.zig
+++ src/ui.zig
@@ -230,14 +230,12 @@ pub const Cursor = union(CursorType) {
 pub const UI = struct {
     tables: ArrayList(Table),
     active_cursor: ?Cursor,
-    hovered_cursor: ?Cursor,
     units: Units,
 
     pub fn init(allocator: Allocator, units: Units) UI {
         return .{
             .tables = ArrayList(Table).initCapacity(allocator, 0) catch unreachable,
             .active_cursor = null,
-            .hovered_cursor = null,
             .units = units,
         };
     }
