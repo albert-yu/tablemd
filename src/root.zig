912e67d55cc7d7002a24d5f7e8e6b42237e8ff4a
Albert Yu <albert.yu@gmail.com>
2025-07-30 21:45:13 -0400

    Decrease touch threshold

--- src/root.zig
+++ src/root.zig
@@ -38,7 +38,7 @@ const BG_COLOR: Color = theme.DARK_THEME.background_color;
 
 const WIDTH_START = 800;
 const HEIGHT_START = 600;
-const TOUCH_THRESHOLD = 10.0; // pixels
+const TOUCH_THRESHOLD = 1.0; // pixels
 
 // Momentum constants
 const MOMENTUM_MIN_SPEED = 5.0; // Minimum speed to trigger momentum
